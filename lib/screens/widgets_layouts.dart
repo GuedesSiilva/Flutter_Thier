8185087ce94af803683556e31526541f00fba3c9
GuedesSiilva <davig0400@gmail.com>
2026-03-06 11:01:33 -0300

    Segundo save do thier

--- lib/screens/widgets_layouts.dart
+++ lib/screens/widgets_layouts.dart
@@ -1,3 +1,4 @@
+import 'package:aula_01/widgets/titulo_sessao.dart';
 import 'package:flutter/material.dart';
 
 class WidgetsLayout extends StatelessWidget {
@@ -10,13 +11,80 @@ class WidgetsLayout extends StatelessWidget {
       body: ListView(
         padding: EdgeInsets.all(16),
         children: [
-          ElevatedButton(
-            onPressed: () => Navigator.push(
-              context,
-              MaterialPageRoute(builder: (_) => WidgetsLayout()),
+          TituloSessao(titulo: "Padding"),
+          Container(
+            color: const Color.fromARGB(255, 32, 109, 209),
+            child: Padding(
+              padding: EdgeInsets.all(20),
+              child: Text("Texto com padding de 20px"),
             ),
-            child: Text("Widgets de Layout"),
           ),
+          Divider(),
+          TituloSessao(titulo: "Align"),
+          Container(
+            height: 80,
+            color: const Color.fromARGB(255, 0, 204, 255),
+            alignment: Alignment.topCenter,
+            child: Text("Alinhamento de texto"),
+          ),
+          Divider(),
+          TituloSessao(titulo: 'Center'),
+          Container(
+            height: 80,
+            color: const Color.fromARGB(255, 33, 255, 100),
+            child: Center(child: Text('Alinhamento de texto')),
+          ),
+          Divider(),
+          TituloSessao(titulo: "Sizebox"),
+          Column(
+            children: [
+              Text("Texto acima"),
+              SizedBox(height: 20),
+              Text("Texto abaixo"),
+            ],
+          ),
+          Divider(),
+          TituloSessao(titulo: 'Expanded e Flexible (em Column)'),
+          Container(
+            height: 200,
+            color: Colors.amberAccent,
+            child: Column(
+              children: [
+                Expanded(
+                  child: Container(
+                    color: Colors.red,
+                    child: Center(child: Text('Expanded')),
+                  ),
+                ),
+                Flexible(
+                  flex: 1,
+                  child: Container(
+                    color: Colors.deepPurple,
+                    child: Text("Flexible (Flex 2)"),
+                  ),
+                )
+              ], // children
+            ),
+          ),
+          Divider(),
+          TituloSessao(titulo: "Expanded e Flexible (Row)"),
+          Row(children: [
+            Expanded(child: Container(
+              height: 50,
+              color: Colors.yellow,
+              child: Center(child: Text("Expanded")),
+            ),
+            ),
+            Flexible(
+              flex: 2,
+              child: Container(
+                height: 50,
+                color: const Color.fromARGB(255, 212, 14, 230),
+                child: Center(child: Text("Flexible (Flex 2)")),
+              ),
+            )
+          ],
+          )
         ], // Children
       ),
     );
