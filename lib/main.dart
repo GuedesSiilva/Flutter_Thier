f56c35ff4c6ed14535daf508241fbd899a5d5215
GuedesSiilva <davig0400@gmail.com>
2026-03-13 11:13:59 -0300

    flutter

--- lib/main.dart
+++ lib/main.dart
@@ -9,36 +9,85 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(    
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home:  ListContents()
-        );
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      home: ListContents(),
+    );
   }
 }
 
 class ListContents extends StatelessWidget {
-  const ListContents({super.key});
+  final List<CatalogoItem> secoes  = [
+    CatalogoItem(
+      titulo: "Widgets de conteúdo",
+      icone: Icons.text_fields,
+      descricao:
+          "Exemplos de widgets de conteúdo como Text, Image, Icon e ElevatedButton",
+      destino: WidgetsConteudo(),
+    ),
+    CatalogoItem(
+      titulo: "Widgets de Layout",
+      icone: Icons.view_quilt,
+      descricao:
+          "Exemplos de widgets de layout como Padding, Align, Center, SizedBox, Expanded e Flexible",
+      destino: WidgetsLayout(),
+    ),
+  ];
+  ListContents({super.key});
 
   @override
-  Widget build(BuildContext context) {
+ Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("Catálogo de widgets"),),
-        body:  Column( 
-          children: [
-            ElevatedButton(onPressed: () => Navigator.push(
-            context,MaterialPageRoute(builder: (_) => WidgetsConteudo(),)),
-            child: Text("Widgets de conteudo")),
-                        ElevatedButton(onPressed: () => Navigator.push(
-            context,MaterialPageRoute(builder: (_) => WidgetsLayout(),)),
-            child: Text("Widgets de Layout")),
-          ],
-        )
-    );
+      appBar: AppBar(title: Text("Catálogo de widget")),
+      body: Column(
+        children: secoes
+            .map(
+              (item) => GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (_) => item.destino),
+                  );
+                },
+              
+              child: Card(
+                elevation: 4,
+                child: Padding(
+                  padding: EdgeInsets.all(12),
+                  child: Column(
+                    children: [
+                      Icon(item.icone, size: 48, color: Colors.blue),
+                      Text(item.titulo, textAlign: TextAlign.center),
+                      Text(
+                        item.descricao,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 12, color: Colors.amber),
+                      ), // Text
+                    ],
+                  ), // Column
+                ), // Padding
+              ), // Card
+            ),
+            )
+            .toList(),
+      ), // Column
+    ); // Scaffold
   }
+}
+
+class CatalogoItem {
+  final String titulo;
+  final IconData icone;
+  final String descricao;
+  final Widget destino;
+
+  CatalogoItem({
+    required this.titulo,
+    required this.icone,
+    required this.descricao,
+    required this.destino,
+  });
 }
